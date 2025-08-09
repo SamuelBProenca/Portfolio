72873c5cf139a9ccddd98332eab01fb56409ab83
samuel <samuelbrito782@gmail.com>
2025-08-09 17:33:20 -0300

    just a test

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -8,7 +8,7 @@ import Contact from './components/contact';
 import Experiences from './components/experiences';
 import About from './components/about';
 import Projects from './components/projects';
-// import FallingLeaves from './components/FallingLeaves';
+import FallingLeaves from './components/FallingLeaves';
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
       
       {/* Folhas caindo - ficam no fundo, mas acima do background */}
       <div className="absolute inset-0 z-0">
-        {/* <FallingLeaves /> */}
+        <FallingLeaves />
       </div>
 
       {/* Efeitos de luzes */}
@@ -24,9 +24,9 @@ export default function Home() {
         aria-hidden
         className="pointer-events-none fixed inset-0 z-0 overflow-hidden"
       >
-        <div className="absolute top-[120px] -left-24 w-[400px] h-[400px] rounded-full bg-purple-500 opacity-20 blur-2xl animate-pulse" />
+        {/* <div className="absolute top-[120px] -left-24 w-[400px] h-[400px] rounded-full bg-purple-500 opacity-20 blur-2xl animate-pulse" />
         <div className="absolute bottom-0 right-0 w-[400px] h-[400px] rounded-full bg-cyan-400 opacity-25 blur-2xl animate-ping delay-1000" />
-        <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 w-[350px] h-[350px] rounded-full bg-emerald-400 opacity-20 blur-2xl animate-pulse delay-2000" />
+        <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 w-[350px] h-[350px] rounded-full bg-emerald-400 opacity-20 blur-2xl animate-pulse delay-2000" /> */}
       </div>
 
       {/* Conteúdo */}
