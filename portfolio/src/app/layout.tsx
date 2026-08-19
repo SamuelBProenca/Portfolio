af9cedf1837b7737c01709dafed7bb7daf73157c
samuel <samuelbrito782@gmail.com>
2026-08-19 14:52:15 -0300

    deleted files

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,17 +1,11 @@
 `use client`;
 import type { Metadata } from "next";
-// import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 // import HeaderComponent from "./components/header/index";
 // import FooterComponent from "./components/footer";
 import ContactWP from "./assets/whatsapp";
 import { ThemeProvider } from "next-themes";
-// import FallingLeaves from "./components/FallingLeaves";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Samuel Proença - Portfolio",
@@ -23,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${geistMono.variable} scroll-smooth`}>
+    <html lang="en" className="scroll-smooth">
       <body className="relative overflow-hidden">
         
 
